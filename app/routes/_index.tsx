5417dcd02da0c4e296f526bad5806cff621a3f76
Quentin Fox <quentinbennettfox@gmail.com>
2024-08-05 10:53:38 -0400

    move _index code to locations

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,45 +1,13 @@
-import type {
-  ActionFunctionArgs,
-  LoaderFunctionArgs,
-} from "@remix-run/cloudflare";
+import type { ActionFunctionArgs } from "@remix-run/cloudflare";
 import { Form, useLoaderData } from "@remix-run/react";
+
 import invariant from "tiny-invariant";
 import { slugify } from "~/helpers/url";
 
-import * as models from "~/models";
-
-export const loader = async ({ context }: LoaderFunctionArgs) => {
-  const { DB } = context.cloudflare.env;
-  const locations = await models.locations.list(DB);
-
-  return { locations };
-};
-
 export const action = async ({ request, context }: ActionFunctionArgs) => {
-  const { DB } = context.cloudflare.env;
-
-  const formData = await request.formData();
-
-  const title = formData.get("title");
-
-  invariant(typeof title === "string");
-
-  const createdAt = Date.now();
-
-  const slug = slugify(title).substring(0, 30);
-
-  const createdAtCode = Buffer.from(createdAt.toString()).toString("base64");
-
-  const publicId = `loc_${slug}_${createdAtCode}`;
-
-  await models.locations.insert(DB, { createdAt, publicId, title });
-
-  // no redirect, but should reload
-  return null;
 };
 
 export default function Index() {
-  const loaderData = useLoaderData<typeof loader>();
 
   return (
     <div>
