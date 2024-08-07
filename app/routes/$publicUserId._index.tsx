f1dd61f7a5d182a3e96926c6eca0dc667700924a
Quentin Fox <quentinbennettfox@gmail.com>
2024-08-07 00:08:32 -0400

    planning for home

--- app/routes/$publicUserId._index.tsx
+++ app/routes/$publicUserId._index.tsx
@@ -3,5 +3,9 @@ import { useOutletUserContext } from "@/context";
 export default function Page() {
   const { user } = useOutletUserContext();
 
+  // design:
+
+  // one full-width card at the top showing the ICS link
+  // two full width cards showing # upcoming shifts, # total shifts
   return <h1 className="text-3xl font-bold">Hello {user.firstName}!</h1>;
 }
