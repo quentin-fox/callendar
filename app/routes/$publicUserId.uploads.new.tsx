0e544a76b75e0d92a71c4cbe2638b8248c89e216
Quentin Fox <quentinbennettfox@gmail.com>
2024-08-11 13:39:03 -0400

    switch to new file uploader component

--- app/routes/$publicUserId.uploads.new.tsx
+++ app/routes/$publicUserId.uploads.new.tsx
@@ -4,12 +4,15 @@ import * as models from "@/models";
 import * as services from "@/services";
 import * as dtos from "@/dtos";
 
-import { json, LoaderFunctionArgs } from "@remix-run/server-runtime";
+import {
+  ActionFunctionArgs,
+  json,
+  LoaderFunctionArgs,
+} from "@remix-run/server-runtime";
 
 import invariant from "tiny-invariant";
 import { validate } from "uuid";
 import { Form, useLoaderData } from "@remix-run/react";
-import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import {
   Select,
@@ -18,7 +21,15 @@ import {
   SelectTrigger,
   SelectItem,
 } from "@/components/ui/select";
-import { FileUploadZone } from "@/components/FileUploadZone";
+import {
+  FileUploader,
+  FileUploaderContent,
+  FileInput,
+} from "@/components/ui/extension/file-uploader";
+import { useCallback, useEffect, useState } from "react";
+import { DropzoneOptions } from "react-dropzone-esm";
+import { Button } from "@/components/ui/button";
+import { TrashIcon } from "@radix-ui/react-icons";
 
 export const loader = async ({ context, params }: LoaderFunctionArgs) => {
   const { DB } = context.cloudflare.env;
@@ -53,19 +64,108 @@ export const loader = async ({ context, params }: LoaderFunctionArgs) => {
   return json({ locations });
 };
 
+export const action = async ({ request }: ActionFunctionArgs) => {
+  const fd = await request.formData();
+
+  console.log([...fd.keys()]);
+
+  return json({ value: null });
+};
+
 export default function Page() {
   const { locations } = useLoaderData<typeof loader>();
+
+  const [uploads, setUploads] = useState<{ file: File; url: string }[]>([]);
+
+  const dropZoneConfig = {
+    accept: {
+      "image/*": [".jpg", ".jpeg", ".png"],
+    },
+    multiple: true,
+    maxFiles: 4,
+    maxSize: 1 * 1024 * 1024,
+  } satisfies DropzoneOptions;
+
+  const handleChangeFiles = useCallback((files: File[] | null) => {
+    if (!files) {
+      setUploads([]);
+      return;
+    }
+
+    const newUploads = files.map((file) => ({
+      file,
+      url: URL.createObjectURL(file),
+    }));
+
+    setUploads((prevUploads) => [...prevUploads, ...newUploads]);
+  }, []);
+
+  const handleRemoveUpload = useCallback((url: string) => {
+    setUploads((currUploads) =>
+      currUploads.filter((upload) => upload.url !== url),
+    );
+  }, []);
+
+  useEffect(() => {
+    const cb = (event: ClipboardEvent) => {
+      if (!event.clipboardData?.files) {
+        return;
+      }
+
+      const { files } = event.clipboardData;
+      handleChangeFiles([...files]);
+    };
+
+    window.addEventListener("paste", cb);
+
+    return () => {
+      window.removeEventListener("paste", cb);
+    };
+  }, [handleChangeFiles]);
 
   return (
     <Form method="POST" encType="multipart/form-data">
       <fieldset>
-        <FileUploadZone />
+        <FileUploader
+          value={uploads.map((u) => u.file)}
+          onValueChange={handleChangeFiles}
+          dropzoneOptions={dropZoneConfig}
+          className="gap-8"
+        >
+          <FileInput className="flex flex-col items-center justify-center rounded-xl border border-dashed border-muted px-8 py-12 hover:border-muted-foreground transition">
+            <p className="text-xl font-bold">Upload an Image</p>
+            <p className="text-sm text-muted-foreground">
+              Click anywhere, paste, or drag-and-drop to upload an image
+            </p>
+          </FileInput>
+          <FileUploaderContent className="flex flex-row flex-wrap justify-center gap-4">
+            {uploads.map((u) => (
+              <div className="group relative" key={u.url}>
+                <img
+                  key={u.url}
+                  className="size-36 rounded-xl object-cover my-4 border border-muted"
+                  alt="schedule screenshot"
+                  src={u.url}
+                />
+                <div className="absolute inset-0 flex items-center justify-center">
+                  <Button
+                    variant="outline"
+                    onClick={() => handleRemoveUpload(u.url)}
+                    size="icon"
+                  >
+                    <TrashIcon />
+                  </Button>
+                </div>
+              </div>
+            ))}
+          </FileUploaderContent>
+        </FileUploader>
       </fieldset>
 
       <fieldset>
         <Label htmlFor="location">Location</Label>
         <Select>
-          <SelectTrigger className="w-[180px]">
+          <SelectTrigger>
             <SelectValue placeholder="Location" />
           </SelectTrigger>
           <SelectContent>
@@ -76,8 +176,9 @@ export default function Page() {
             ))}
           </SelectContent>
         </Select>
-        <Input type="file" name="file" />
       </fieldset>
+
+      <Button type="submit">Upload</Button>
     </Form>
   );
 }
