89596966e634d0bd462045299b765d2d821873a4
Quentin Fox <quentinbennettfox@gmail.com>
2024-09-19 10:57:28 +0200

    use fetcher for first form on page, since no navigation happens

--- app/routes/$publicUserId.uploads.new.tsx
+++ app/routes/$publicUserId.uploads.new.tsx
@@ -3,16 +3,8 @@ import { isError } from "@/helpers/result";
 import * as models from "@/models";
 import * as services from "@/services";
 import * as dtos from "@/dtos";
-import * as entities from "@/entities";
-import * as adapters from "@/adapters";
 
-import {
-  ActionFunctionArgs,
-  json,
-  LoaderFunctionArgs,
-  unstable_createMemoryUploadHandler,
-  unstable_parseMultipartFormData,
-} from "@remix-run/server-runtime";
+import { json, LoaderFunctionArgs } from "@remix-run/server-runtime";
 
 import invariant from "tiny-invariant";
 import { validate } from "uuid";
@@ -20,7 +12,7 @@ import {
   Form,
   useLoaderData,
   useBeforeUnload,
-  useActionData,
+  useFetcher,
 } from "@remix-run/react";
 import { Label } from "@/components/ui/label";
 import {
@@ -95,86 +87,23 @@ export const loader = async ({ context, params }: LoaderFunctionArgs) => {
   return json({ locations });
 };
 
-export const action = async ({ request }: ActionFunctionArgs) => {
-  const uploadHandler = unstable_createMemoryUploadHandler({
-    maxPartSize: 5_000_000,
-  });
-
-  const formData = await unstable_parseMultipartFormData(
-    request,
-    uploadHandler,
-  );
-
-  const name = formData.get("name");
-  invariant(typeof name === "string");
-
-  const extra = formData.get("extra");
-  invariant(typeof extra === "string" || extra === null);
-
-  const images = formData.getAll("images[]");
-
-  // to be uploaded to claude
-  const contents: { data: string; mediaType: entities.ValidMediaType }[] =
-    await Promise.all(
-      images.flatMap((image) => {
-        if (image instanceof File === false) {
-          return [];
-        }
-
-        const matchingValidMediaType = entities.validMediaTypes.find(
-          (mt) => mt === image.type,
-        );
-
-        if (!matchingValidMediaType) {
-          return [];
-        }
-
-        return image.arrayBuffer().then((buffer) => {
-          const data = Buffer.from(buffer).toString("base64");
-          return {
-            data,
-            mediaType: matchingValidMediaType,
-          };
-        });
-      }),
-    );
-
-  const generateShifts = adapters.mock.generateShifts;
-
-  const result = await services.uploads.process(generateShifts, {
-    name,
-    extra,
-    contents,
-  });
-
-  if (isError(result)) {
-    return json({
-      shifts: null,
-      receivedAt: Date.now(),
-      error: result.error.join("\n"),
-    });
-  }
-
-  if (result.value.length === 0) {
-    return json({
-      shifts: null,
-      receivedAt: Date.now(),
-      error: "No shifts could be created. Please try uploading another image.",
-    });
-  }
-
-  const shifts: (dtos.AllDayShiftOutput | dtos.TimedShiftOutput)[] =
-    result.value;
-
-  return json({ shifts, receivedAt: Date.now(), error: null });
-};
-
 export default function Page() {
   const { locations } = useLoaderData<typeof loader>();
 
   const [uploads, setUploads] = useState<{ file: File; url: string }[]>([]);
 
-  const actionData = useActionData<typeof action>();
+  const processUploadFetcher = useFetcher<
+    | {
+        shifts: (dtos.AllDayShiftOutput | dtos.TimedShiftOutput)[];
+        receivedAt: number;
+        error: null;
+      }
+    | {
+        shifts: null;
+        receivedAt: number;
+        error: string;
+      }
+  >();
 
   const dropZoneConfig = {
     accept: {
@@ -218,11 +147,13 @@ export default function Page() {
 
   return (
     <>
-      {actionData && actionData.error && (
-        <ErrorAlert error={actionData.error} />
+      {processUploadFetcher.data && processUploadFetcher.data.error && (
+        <ErrorAlert error={processUploadFetcher.data.error} />
       )}
-      <Form
+      <processUploadFetcher.Form
         method="POST"
+        relative="path"
+        action="../process"
         encType="multipart/form-data"
         className="flex flex-col gap-4"
       >
@@ -273,16 +204,19 @@ export default function Page() {
           <Textarea name="extra" />
         </fieldset>
 
-        {(!actionData || actionData.error) && (
-          <Button type="submit">Upload</Button>
-        )}
-      </Form>
-
-      {actionData && actionData.shifts && (
+        <Button type="submit" disabled={!!processUploadFetcher.data?.shifts}>
+          {processUploadFetcher.data?.shifts
+            ? "Uploaded"
+            : processUploadFetcher.state === "idle"
+              ? "Upload"
+              : "Uploading..."}
+        </Button>
+      </processUploadFetcher.Form>
+      {!!processUploadFetcher.data?.shifts && (
         <ShiftsForm
-          key={actionData.receivedAt}
+          key={processUploadFetcher.data.receivedAt}
           locations={locations}
-          shifts={actionData.shifts}
+          shifts={processUploadFetcher.data.shifts}
         />
       )}
     </>
