d12acd73cc91cd03e251d94d81beac233e6c6435
Quentin Fox <quentinbennettfox@gmail.com>
2024-09-23 20:11:33 +0200

    add middleware for asserting user exists

--- app/loaders/user.server.ts
+++ app/loaders/user.server.ts
@@ -1,31 +1,11 @@
 import { json, LoaderFunctionArgs } from "@remix-run/server-runtime";
 
-import invariant from "tiny-invariant";
-import { validate } from "uuid";
+import { middleware } from "@/middleware/user.server";
 
-import * as models from "@/models";
-
-import * as entities from "@/entities";
 import * as dtos from "@/dtos";
 
-export const loader = async ({ params, context }: LoaderFunctionArgs) => {
-  const publicUserId = params.publicUserId;
-
-  invariant(publicUserId, "publicUserId not found");
-
-  if (!validate(publicUserId)) {
-    throw new Error("publicUserID must be a valid UUID");
-  }
-
-  const { DB } = context.cloudflare.env;
-
-  const listOneUser = models.users.listOne.bind(null, DB);
-
-  const user: entities.User | null = await listOneUser({ publicUserId });
-
-  if (!user) {
-    throw new Error("User does not exist.");
-  }
+export const loader = async (args: LoaderFunctionArgs) => {
+  const user = await middleware(args);
 
   const response: { user: dtos.User } = {
     user: {
---
+++ app/middleware/user.server.ts
@@ -0,0 +1,38 @@
+import { AppLoadContext } from "@remix-run/server-runtime";
+
+import invariant from "tiny-invariant";
+import { validate } from "uuid";
+
+import * as models from "@/models";
+
+import * as entities from "@/entities";
+import { Params } from "@remix-run/react";
+
+export const middleware = async ({
+  params,
+  context,
+}: {
+  // simplify the required types so it can be used in both actions/loaders w/o issues
+  params: Params;
+  context: AppLoadContext;
+}): Promise<entities.User> => {
+  const publicUserId = params.publicUserId;
+
+  invariant(publicUserId, "publicUserId not found");
+
+  if (!validate(publicUserId)) {
+    throw new Error("publicUserID must be a valid UUID");
+  }
+
+  const { DB } = context.cloudflare.env;
+
+  const listOneUser = models.users.listOne.bind(null, DB);
+
+  const user: entities.User | null = await listOneUser({ publicUserId });
+
+  if (!user) {
+    throw new Error("User does not exist.");
+  }
+
+  return user;
+};
